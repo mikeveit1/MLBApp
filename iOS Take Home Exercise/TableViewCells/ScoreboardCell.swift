53c058f2c82ea1da65841d5eb521b9a4b50ea702
Michael Veit <mikeveit1@gmail.com>
2021-10-08 16:12:45 -0400

    set up cell

--- iOS Take Home Exercise/TableViewCells/ScoreboardCell.swift
+++ iOS Take Home Exercise/TableViewCells/ScoreboardCell.swift
@@ -9,15 +9,30 @@
 import UIKit
 
 class ScoreboardCell: UITableViewCell {
-
+    
+    @IBOutlet weak var gameStackView: UIStackView!
+    @IBOutlet weak var teamStackView: UIStackView!
+    @IBOutlet weak var awayStackView: UIStackView!
+    @IBOutlet weak var awayNameLabel: UILabel!
+    @IBOutlet weak var awayRecordLabel: UILabel!
+    @IBOutlet weak var homeStackView: UIStackView!
+    @IBOutlet weak var homeNameLabel: UILabel!
+    @IBOutlet weak var homeRecordLabel: UILabel!
+    @IBOutlet weak var scoreStackView: UIStackView!
+    @IBOutlet weak var awayScoreLabel: UILabel!
+    @IBOutlet weak var homeScoreLabel: UILabel!
+    @IBOutlet weak var inningStackView: UIStackView!
+    @IBOutlet weak var inningLabel: UILabel!
+    
+    
     override func awakeFromNib() {
         super.awakeFromNib()
         setUpViews()
     }
-
+    
     override func setSelected(_ selected: Bool, animated: Bool) {
         super.setSelected(selected, animated: animated)
-
+        
         // Configure the view for the selected state
     }
     
@@ -27,10 +42,59 @@ class ScoreboardCell: UITableViewCell {
     
     private func setUpViews() {
         configureView()
+        configureGameStackView()
     }
     
     private func configureView() {
         self.backgroundColor = Colors.backgroundColor
     }
-
+    
+    private func configureGameStackView() {
+        configureTeamStackView()
+        configureScoreStackView()
+        configureInningStackView()
+    }
+    
+    private func configureTeamStackView() {
+        
+        configureAwayNameLabel()
+        configureAwayRecordLabel()
+        configureHomeNameLabel()
+        configureHomeRecordLabel()
+    }
+    
+    private func configureAwayNameLabel() {
+        
+    }
+    
+    private func configureAwayRecordLabel() {
+        
+    }
+    
+    private func configureHomeNameLabel() {
+        
+    }
+    
+    private func configureHomeRecordLabel() {
+        
+    }
+    
+    private func configureScoreStackView() {
+        let labels = [awayScoreLabel, homeScoreLabel]
+        for label in labels {
+            configureScoreLabel(label: label!)
+        }
+    }
+    
+    private func configureScoreLabel(label: UILabel) {
+        
+    }
+    
+    private func configureInningStackView() {
+        configureInningLabel()
+    }
+    
+    private func configureInningLabel() {
+    
+    }
 }
