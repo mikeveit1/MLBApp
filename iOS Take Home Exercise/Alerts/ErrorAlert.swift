06e0dc02a64a725fe6856eee5779d8cdccdeafad
Michael Veit <mikeveit1@gmail.com>
2021-10-11 16:27:47 -0400

    fixed format

--- iOS Take Home Exercise/Alerts/ErrorAlert.swift
+++ iOS Take Home Exercise/Alerts/ErrorAlert.swift
@@ -13,7 +13,7 @@ extension UIViewController {
     public func showErrorAlert(title: String, message:String ) {
         let alertController = UIAlertController(title: title, message: message, preferredStyle: .alert)
         let defaultAction = UIAlertAction(title: "OK", style: .cancel, handler: { action in
-                                            self.dismiss(animated: true, completion: nil) })
+            self.dismiss(animated: true, completion: nil) })
         alertController.addAction(defaultAction)
         self.present(alertController, animated: true, completion: nil)
     }
